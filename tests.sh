20cdd32f5cfae031581cf2b904279b0a9dfbcae3
“Jennifer <jevans@veryrealemail.com>
2014-01-24 11:12:06 +1100

    added tests

---
+++ tests.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+TEST=`grep "With group packages starting for teams of four all the way up to 30, no other travel provider gets your group the distance" 'Teams In Space.html'`
+
+if [ -n "${TEST}" ]; then
+  exit 0
+fi
+
+exit 1
+
