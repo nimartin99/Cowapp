3826fb13637f893a1676c7021bee51ca94b4b11a
TLeibl <tabea.leibl@web.de>
2020-10-14 10:13:36 +0200

    added activities for all screens

---
+++ app/src/main/java/de/monokel/frontend/PushNotificationActivity.java
@@ -0,0 +1,14 @@
+package de.monokel.frontend;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PushNotificationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_push_notification);
+    }
+}
---
+++ app/src/main/java/de/monokel/frontend/ReportInfectionActivity.java
@@ -0,0 +1,14 @@
+package de.monokel.frontend;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ReportInfectionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_report_infection);
+    }
+}
