d2294e183f43137ecfcdeee1b34d0d00f21af0ca
Philipp Alessandrini <palessan@stud.hs-heilbronn.de>
2020-10-21 17:27:04 +0200

    extended utility-script to also delete the 'infected' collection

--- server/utils/reset_database.js
+++ server/utils/reset_database.js
@@ -3,7 +3,7 @@
  *
  * @author Mergim Miftari
  * @author Philipp Alessandrini
- * @version 2020-10-18
+ * @version 2020-10-21
  */
 
 // init mongoDB
@@ -11,17 +11,23 @@ const MongoClient = require('mongodb').MongoClient;
 const url = "mongodb+srv://admin:zyI8ZX5zmAyfjaVt@cowapp.9hh4n.mongodb.net/CoWApp?retryWrites=true&w=majority";
 
 // connect to the database
-MongoClient.connect(url, { useUnifiedTopology: true }, function(err, db) {
+MongoClient.connect(url, { useUnifiedTopology: true }, async function(err, db) {
     if (err) {
         console.log("Error while connecting mongo client");
     } else {
         const cowappDb = db.db('CoWAppDB');
         const keyCollection = cowappDb.collection('key');
-        // reset key-counter
+        const infectedCollection = cowappDb.collection('infected');
+        // reset key-counter in 'key' collection
         const zerocounter = { $set: {key: "0"} };
-        keyCollection.updateOne({}, zerocounter, function(err, res) {
+        await keyCollection.updateOne({}, zerocounter, function(err, res) {
             if (err) throw err;
             console.log("Key-Counter successfully reset");
+        });
+        // drop 'infected' collection
+        await infectedCollection.drop(function(err, delOK) {
+            if (err) console.log("There are no infected keys reported at the moment");
+            if (delOK) console.log("Infected keys successfully deleted");
             db.close();
         });
     }
