a6774a8f4923006e2916137b8d5a1552cb64a765
Mergim Miftari <mmiftari@stud.hs-heilbronn.de>
2020-10-29 12:36:16 +0100

    added description to the Alarm class

--- app/src/main/java/de/monokel/frontend/provider/Alarm.java
+++ app/src/main/java/de/monokel/frontend/provider/Alarm.java
@@ -1,7 +1,15 @@
 package de.monokel.frontend.provider;
 
+/**
+ * This class has the method which is called once a day.
+ *
+ * @author Miftari
+ */
 public class Alarm {
 
+    /**
+     * This method is called once a day.
+     */
     public static void dailyBusiness() {
         //delete all keys older then 3 weeks.
         LocalKeySafer.addKeyPairToSavedKeyPairs(null);
