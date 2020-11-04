1035e15ea811bed67dc15948311c6aa7566085dc
Nico <nimartin@stud.hs-heilbronn.de>
2020-11-04 17:56:04 +0100

    Constants override to making app work on virtual devices

--- app/src/main/java/de/monokel/frontend/Constants.java
+++ app/src/main/java/de/monokel/frontend/Constants.java
@@ -4,9 +4,13 @@ public class Constants {
     // Debug flag for testing purposes
     public static final boolean DEBUG = true;
     // Flag for controlling background scanning and transmitting (BLE Beacon)
-    public static final boolean SCAN_AND_TRANSMIT = true;
+    // Set false if you try to run the app on a virtual device (emulator)
+    public static final boolean SCAN_AND_TRANSMIT = false;
     // The time difference between device scans in ms
     public static final Long BACKGROUND_SCAN_PERIOD = 1100L;
     public static final Long FOREGROUND_SCAN_PERIOD = 1100L;
-    public static final String id1 = "01234567-0506-0708-090a-0b0c0d0e0f10";
+    public static final String id1 = "01234567-0506-0708-090a-222222222222";
+    // 000000000000
+    // 111111111111
+    // 222222222222
 }
