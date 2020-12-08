62bab48f6acf89156752e5b40b9a7a566fb15bb7
Jonas <jklein@stud.hs-heilbronn.de>
2020-12-08 18:11:39 +0100

    link inside the TextGeneralInfo TextView changed to clickable

--- app/src/main/java/de/hhn/frontend/InfectionActivity.java
+++ app/src/main/java/de/hhn/frontend/InfectionActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.util.Linkify;
+import android.widget.TextView;
 
 /**
  * Information for currently infected people using the CoWApp
@@ -17,9 +19,11 @@ public class InfectionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //logo of the app in the action bar
-        ActionBar actionBar= getSupportActionBar();
+        ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayShowHomeEnabled(true);
         actionBar.setIcon(R.mipmap.ic_cowapp);
         setContentView(R.layout.activity_infection);
+        TextView generalInfoTextView = (TextView) findViewById(R.id.TextGeneralInfo);
+        Linkify.addLinks(generalInfoTextView, Linkify.ALL);
     }
 }
