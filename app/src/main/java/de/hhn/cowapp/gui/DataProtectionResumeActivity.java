dac49b225eaf81f979d88044e8727bd9e02554c9
TLeibl <tabea.leibl@web.de>
2020-12-14 23:05:51 +0100

    123 - added terms of use to info menu to re-read for the user

---
+++ app/src/main/java/de/hhn/cowapp/gui/DataProtectionResumeActivity.java
@@ -0,0 +1,22 @@
+package de.hhn.cowapp.gui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import de.hhn.cowapp.R;
+
+/**
+ * Data protection to re-read
+ *
+ * @author Tabea leibl
+ * @version 2020-12-14
+ */
+public class DataProtectionResumeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_data_protection_resume);
+    }
+}
